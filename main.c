9b396d1560e7a1e2a9137002e68eca1931a27deb
Hector Gil Marin <hectorgil@users.noreply.github.com>
2020-09-14 12:44:42 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -55,7 +55,7 @@ double Pb2_delta,Pb2_theta,Pbs2_delta,Pbs2_theta,Pb2s2,Pbs22,Pb22,sigma3;
 double *Pk13,*Pk14,*Pk15,*Pk16,*Pk17,*Pk18,*Pk19,*Pk20;
 
 double Taruya_A11,Taruya_A12,Taruya_A22,Taruya_A23,Taruya_A33,Taruya_B1_11,Taruya_B1_12,Taruya_B1_21,Taruya_B1_22,Taruya_B2_11,Taruya_B2_12,Taruya_B2_21,Taruya_B2_22,Taruya_B3_12,Taruya_B3_21,Taruya_B3_22,Taruya_B4_22;
-double *Pk21,*Pk22,*Pk23,*Pk24,*Pk25,*Pk26,*Pk27,*Pk28,*Pk29,*Pk30,*Pk31,*Pk32,*Pk33,*Pk34,*Pk35,*Pk36,*Pk37;
+double *Pk21,*Pk22,*Pk23,*Pk24,*Pk25,*Pk26,*Pk27,*Pk28,*Pk29,*Pk30,*Pk31,*Pk32,*Pk33,*Pk34,*Pk35,*Pk36,*Pk37,*Pk38,*Pk39,*Pk40,*Pk41;
 
 double sigma8;
 
@@ -211,13 +211,17 @@ Pk34 = (double*) calloc( imax, sizeof(double));
 Pk35 = (double*) calloc( imax, sizeof(double));
 Pk36 = (double*) calloc( imax, sizeof(double));
 Pk37 = (double*) calloc( imax, sizeof(double));
+Pk38 = (double*) calloc( imax, sizeof(double));
+Pk39 = (double*) calloc( imax, sizeof(double));
+Pk40 = (double*) calloc( imax, sizeof(double));
+Pk41 = (double*) calloc( imax, sizeof(double));
 
 
         f_params *function_parameters;
 time_ini=time(NULL);
 printf("Performing integrals. This may take a while...\n");
 
-#pragma omp parallel for private(sigma8,function_parameters,tid,i,k,p0,p22dd,p22do,p22oo,p33dd,p33do,p33oo,p13d,p13o,p15d,p15o,p24dd,p24oo,p24do1,p24do2,Pb2_delta,Pb2_theta,Pbs2_delta,Pbs2_theta,Pb2s2,Pbs22,Pb22,sigma3,Taruya_A11,Taruya_A12,Taruya_A22,Taruya_A23,Taruya_A33,Taruya_B1_11,Taruya_B1_12,Taruya_B1_21,Taruya_B1_22,Taruya_B2_11,Taruya_B2_12,Taruya_B2_21,Taruya_B2_22,Taruya_B3_12,Taruya_B3_21,Taruya_B3_22,Taruya_B4_22,trash) shared(interval,Nlines,klin,Plin,Plin2pi3,imax,kmin,precision_rpt,precision_max,precision_nlb,precision_tns,XMIN33,XMAX33,XMIN22,XMAX22,XMIN13,XMAX13,XMAX22alt,XMIN44,XMAX44,XMIN,XMAX,XMAXalt,Pk1,Pk2,Pk3,Pk4,Pk5,Pk6,Pk7,Pk8,Pk9,Pk10,Pk11,Pk12,Pk13,Pk14,Pk15,Pk16,Pk17,Pk18,Pk19,Pk20,Pk21,Pk22,Pk23,Pk24,Pk25,Pk26,Pk27,Pk28,Pk29,Pk30,Pk31,Pk32,Pk33,Pk34,Pk35,Pk36,Pk37,interval_type,kmin1,kmax1,kmax11)
+#pragma omp parallel for private(sigma8,function_parameters,tid,i,k,p0,p22dd,p22do,p22oo,p33dd,p33do,p33oo,p13d,p13o,p15d,p15o,p24dd,p24oo,p24do1,p24do2,Pb2_delta,Pb2_theta,Pbs2_delta,Pbs2_theta,Pb2s2,Pbs22,Pb22,sigma3,Taruya_A11,Taruya_A12,Taruya_A22,Taruya_A23,Taruya_A33,Taruya_B1_11,Taruya_B1_12,Taruya_B1_21,Taruya_B1_22,Taruya_B2_11,Taruya_B2_12,Taruya_B2_21,Taruya_B2_22,Taruya_B3_12,Taruya_B3_21,Taruya_B3_22,Taruya_B4_22,trash) shared(interval,Nlines,klin,Plin,Plin2pi3,imax,kmin,precision_rpt,precision_max,precision_nlb,precision_tns,XMIN33,XMAX33,XMIN22,XMAX22,XMIN13,XMAX13,XMAX22alt,XMIN44,XMAX44,XMIN,XMAX,XMAXalt,Pk1,Pk2,Pk3,Pk4,Pk5,Pk6,Pk7,Pk8,Pk9,Pk10,Pk11,Pk12,Pk13,Pk14,Pk15,Pk16,Pk17,Pk18,Pk19,Pk20,Pk21,Pk22,Pk23,Pk24,Pk25,Pk26,Pk27,Pk28,Pk29,Pk30,Pk31,Pk32,Pk33,Pk34,Pk35,Pk36,Pk37,Pk38,Pk39,Pk40,Pk41,interval_type,kmin1,kmax1,kmax11)
 for(i=0;i<imax;i++)
 {
 tid=omp_get_thread_num();
@@ -242,7 +246,7 @@ k=pow(10,k);
 
 if(i==0){
 adapt_integrate(1, integrals8 , function_parameters, 1, XMIN, XMAXalt ,0, precision_max, precision_max, &sigma8, &trash);
-Pk37[0]=sqrt(sigma8);
+Pk41[0]=sqrt(sigma8);
 }
 
 printf("[%d/%d] k->%lf (threadID=%d)\n",i,imax-1,k,tid);
@@ -286,7 +290,9 @@ printf("[%d/%d] k->%lf (threadID=%d)\n",i,imax-1,k,tid);
         p15d=p15d*pow(2.*Pi,3);
         p15o=p15o*pow(2.*Pi,3);
 
-Pk1[i]=p0;Pk2[i]=p22dd;Pk3[i]=p22do;Pk4[i]=p22oo;Pk5[i]=p33dd;Pk6[i]=p33do;Pk7[i]=p33oo;Pk8[i]=p13d;Pk9[i]=p13o;Pk10[i]=p15d;Pk11[i]=p15o;
+printf("Done with RPT integration of k->%lf on thread %d \n", k, tid);
+
+Pk1[i]=p0;Pk2[i]=p22dd;Pk3[i]=p22do;Pk4[i]=p22oo;Pk5[i]=p33dd;Pk6[i]=p33do;Pk7[i]=p33oo;Pk8[i]=p13d;Pk9[i]=p13o;Pk10[i]=p15d;Pk11[i]=p15o;Pk12[i]=p24dd;Pk13[i]=p24oo;Pk14[i]=p24do1;Pk15[i]=p24do2;
 
 //bias
          adapt_integrate(1,intPb2_delta,function_parameters,2,XMIN22,XMAX22alt,0,precision_nlb,precision_nlb,&Pb2_delta,&trash);
@@ -301,7 +307,9 @@ Pk1[i]=p0;Pk2[i]=p22dd;Pk3[i]=p22do;Pk4[i]=p22oo;Pk5[i]=p33dd;Pk6[i]=p33do;Pk7[i
 
 sigma3=sigma3*P_interpolLOG(k,klin,Plin2pi3,Nlines);
 
-Pk12[i]=Pb2_delta;Pk13[i]=Pb2_theta;Pk14[i]=Pbs2_delta;Pk15[i]=Pbs2_theta;Pk16[i]=Pb2s2;Pk17[i]=Pbs22;Pk18[i]=Pb22;Pk19[i]=sigma3;
+Pk16[i]=Pb2_delta;Pk17[i]=Pb2_theta;Pk18[i]=Pbs2_delta;Pk19[i]=Pbs2_theta;Pk20[i]=Pb2s2;Pk21[i]=Pbs22;Pk22[i]=Pb22;Pk23[i]=sigma3;
+
+printf("Done with NLB integration of k->%lf on thread %d\n", k, tid);
 
 //Taruya terms
                adapt_integrate(1, Function_A11 , function_parameters, 2, XMIN22, XMAX22alt,0, precision_tns, precision_tns, &Taruya_A11, &trash);
@@ -321,9 +329,11 @@ Pk12[i]=Pb2_delta;Pk13[i]=Pb2_theta;Pk14[i]=Pbs2_delta;Pk15[i]=Pbs2_theta;Pk16[i
                 adapt_integrate(1, Function_B3_21 , function_parameters, 2, XMIN22, XMAX22alt,0, precision_tns, precision_tns, &Taruya_B3_21, &trash);
                 adapt_integrate(1, Function_B3_22 , function_parameters, 2, XMIN22, XMAX22alt,0, precision_tns, precision_tns, &Taruya_B3_22, &trash);
                 adapt_integrate(1, Function_B4_22 , function_parameters, 2, XMIN22, XMAX22alt,0, precision_tns, precision_tns, &Taruya_B4_22, &trash);
-Pk20[i]=Taruya_A11;Pk21[i]=Taruya_A12;Pk22[i]=Taruya_A22;Pk23[i]=Taruya_A23;Pk24[i]=Taruya_A33;Pk25[i]=Taruya_B1_11;Pk26[i]=Taruya_B1_12;Pk27[i]=Taruya_B1_21;Pk28[i]=Taruya_B1_22;Pk29[i]=Taruya_B2_11;Pk30[i]=Taruya_B2_12;Pk31[i]=Taruya_B2_21;Pk32[i]=Taruya_B2_22;Pk33[i]=Taruya_B3_12;Pk34[i]=Taruya_B3_21;Pk35[i]=Taruya_B3_22;Pk36[i]=Taruya_B4_22;
+Pk24[i]=Taruya_A11;Pk25[i]=Taruya_A12;Pk26[i]=Taruya_A22;Pk27[i]=Taruya_A23;Pk28[i]=Taruya_A33;Pk29[i]=Taruya_B1_11;Pk30[i]=Taruya_B1_12;Pk31[i]=Taruya_B1_21;Pk32[i]=Taruya_B1_22;Pk33[i]=Taruya_B2_11;Pk34[i]=Taruya_B2_12;Pk35[i]=Taruya_B2_21;Pk36[i]=Taruya_B2_22;Pk37[i]=Taruya_B3_12;Pk38[i]=Taruya_B3_21;Pk39[i]=Taruya_B3_22;Pk40[i]=Taruya_B4_22;
 
 }
+
+printf("Done with TNS integration of k->%lf on thread %d \n", k, tid);
 
 f=fopen(name_output,"w");
 for(i=0;i<imax;i++)
@@ -341,7 +351,7 @@ k=pow(10,k);
 }
 
 
-fprintf(f,"%e %e %e %e %e %e %e %e %e %e %e %e %e %e %e %e %e %e %e %e %e %e %e %e %e %e %e %e %e %e %e %e %e %e %e %e %e %e\n",k,Pk1[i],Pk2[i],Pk3[i],Pk4[i],Pk5[i],Pk6[i],Pk7[i],Pk8[i],Pk9[i],Pk10[i],Pk11[i],Pk12[i],Pk13[i],Pk14[i],Pk15[i],Pk16[i],Pk17[i],Pk18[i],Pk19[i],Pk20[i],Pk21[i],Pk22[i],Pk23[i],Pk24[i],Pk25[i],Pk26[i],Pk27[i],Pk28[i],Pk29[i],Pk30[i],Pk31[i],Pk32[i],Pk33[i],Pk34[i],Pk35[i],Pk36[i],Pk37[0]);
+fprintf(f,"%e %e %e %e %e %e %e %e %e %e %e %e %e %e %e %e %e %e %e %e %e %e %e %e %e %e %e %e %e %e %e %e %e %e %e %e %e %e %e %e %e %e\n",k,Pk1[i],Pk2[i],Pk3[i],Pk4[i],Pk5[i],Pk6[i],Pk7[i],Pk8[i],Pk9[i],Pk10[i],Pk11[i],Pk12[i],Pk13[i],Pk14[i],Pk15[i],Pk16[i],Pk17[i],Pk18[i],Pk19[i],Pk20[i],Pk21[i],Pk22[i],Pk23[i],Pk24[i],Pk25[i],Pk26[i],Pk27[i],Pk28[i],Pk29[i],Pk30[i],Pk31[i],Pk32[i],Pk33[i],Pk34[i],Pk35[i],Pk36[i],Pk37[i],Pk38[i],Pk39[i],Pk40[i],Pk41[0]);
 }
 fclose(f);
 time_fin=time(NULL);
@@ -360,7 +370,7 @@ printf("Computation finised in %lf hours.\n",time_ini/60.);
 
 free(Pk1);free(Pk2);free(Pk3);free(Pk4);free(Pk5);free(Pk6);free(Pk7);free(Pk8);free(Pk9);free(Pk10);free(Pk11);free(Pk12);free(Pk13);free(Pk14);free(Pk15);
 free(Pk16);free(Pk17);free(Pk18);free(Pk19);free(Pk20);free(Pk21);free(Pk22);free(Pk23);free(Pk24);free(Pk25);free(Pk26);free(Pk27);free(Pk28);free(Pk29);free(Pk30);free(Pk31);
-free(Pk32);free(Pk33);free(Pk34);free(Pk35);free(Pk36);free(Pk37);
+free(Pk32);free(Pk33);free(Pk34);free(Pk35);free(Pk36);free(Pk37);free(Pk38);free(Pk39);free(Pk40);free(Pk41);
 free(klin);free(Plin);free(Plin2pi3);
 
 printf("Success! Exiting...\n");
